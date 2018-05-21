fbd0dd663c0e5c19b0d173584380e71b89d020e9
Kranti Nebhwani <darkphoton20@gmail.com>
2018-05-21 22:19:47 +0800

    Updated theme and design, positioning and functionality of about / content page.

--- splash-script.js
+++ splash-script.js
@@ -4,18 +4,18 @@
 
 // global vars
 
-let navContact = document.querySelector('.nav .contact');
-let navAbout = document.querySelector('.nav .about');
-let navResume = document.querySelector('.nav .resume');
-let navProjects = document.querySelector('.nav .projects');
+const navContact = document.querySelector('.nav .contact');
+const navAbout = document.querySelector('.nav .about');
+const navResume = document.querySelector('.nav .resume');
+const navProjects = document.querySelector('.nav .projects');
 
 
 // Show Menu
 
 // vars
-let menuButton = document.getElementById("menu-btn");
-let navMenu = document.querySelector(".nav");
-let closeMenu = document.querySelector(".nav-close");
+const menuButton = document.getElementById("menu-btn");
+const navMenu = document.querySelector(".nav");
+const closeMenu = document.querySelector(".nav-close");
 
 menuButton.addEventListener("click", () => navMenu.classList.add("show"));
 
@@ -49,23 +49,28 @@ closeMenu.addEventListener("click", () => navMenu.classList.remove("show"));
 */
 
 // individual nav buttons
-let overlay = document.querySelector(".overlay");
+const content = document.querySelector(".content");
+const overlay = document.querySelector(".overlay");
 navResume.addEventListener("click", () => overlay.classList.add("show"));
 
 navProjects.addEventListener("click", () => overlay.classList.add("show"));
 navContact.addEventListener("click", () => overlay.classList.add("show"));
+navContact.addEventListener("click", () => content.classList.add("show"))
 
 /* Overlay */
 
-// remove overlay when clicked on to return to the main landing page
-let overlayBtn = document.querySelector(".overlay .close-btn");
-overlayBtn.addEventListener("click", () => overlay.classList.remove("show"));
+// remove overlay and content box when clicked on to return to the main landing page
+const overlayBtn = document.querySelector(".close-btn");
+overlayBtn.addEventListener("click", () => {
+    overlay.classList.remove("show")
+    content.classList.remove("show")
+});
 
 
 
 // About
-let aboutSection = document.getElementById("about"); 
-let closeAbout = document.querySelector("#about .close-about");
+const aboutSection = document.getElementById("about"); 
+const closeAbout = document.querySelector("#about .close-about");
 
 // show about section when about is clicked in nav menu
 navAbout.addEventListener("click", () => aboutSection.classList.add("show"));
@@ -76,6 +81,9 @@ aboutSection.addEventListener("mouseleave", () => closeAbout.classList.remove("s
 
 // close button closes about section
 closeAbout.addEventListener("click", () => aboutSection.classList.remove("show"));
+
+
+// Projects 
 
 
 /* test */
